1ca60f14f37af11a47aa3abf687738870effa087
EstDua <92175044+EstDua@users.noreply.github.com>
2022-01-02 15:11:40 +0100

    +cadenasdetexto

---
+++ cadenaTexto.py
@@ -0,0 +1,11 @@
+###Lectura normal imprime buen dia 
+dato1 ="buen dia"
+print(dato1)
+
+###Debanado imprime solo buen
+dato1 ="buen dia"
+print(dato1[0:4])
+
+###Debanado imprime solo n
+dato1 ="buen dia"
+print(dato1[3:4])
